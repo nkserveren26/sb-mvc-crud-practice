52cc8c250cde8685eb8f711f1378e457316125e9
nkserveren26 <norinoritoshi@outlook.jp>
2024-04-23 23:34:57 +0900

    従業員登録フォーム作成

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -32,4 +32,14 @@ public class EmployeeController {
 
         return "employees/list-employees";
     }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+        // create model attribute to bind form data
+        Employee theEmployee = new Employee();
+
+        theModel.addAttribute("employee", theEmployee);
+
+        return "employees/employee-form";
+    }
 }
